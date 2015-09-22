96561f230902ed97e6a97a68a56ce1300ddde279
Steven Nguyen <ngynstvn@gmail.com>
2015-09-22 01:20:08 -0700

    Integrated Facebook into the application. Added button to the view but will remove it at some point to use an open source library that was recommended in the reading.

--- BlocParty/app/src/main/java/com/ngynstvn/android/blocparty/MainActivity.java
+++ BlocParty/app/src/main/java/com/ngynstvn/android/blocparty/MainActivity.java
@@ -1,37 +1,148 @@
 package com.ngynstvn.android.blocparty;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "(" + MainActivity.class.getSimpleName() + "): ";
+
+    private LoginButton fbLogInButton;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private AccessToken accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e(TAG, "onCreate() called");
         super.onCreate(savedInstanceState);
+
+        // IMPORTANT: Must place this line before setContentView()
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
+
+        // Initialize the Facebook SDK
+
+        // Create the Faceabook callback manager (handles login responses)
+        callbackManager = CallbackManager.Factory.create();
+
+        // Optional: keep track of access token
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken accessToken, AccessToken accessToken1) {
+
+            }
+        };
+
+        accessToken = AccessToken.getCurrentAccessToken();
+
+        // Inflate the login button and set it to certain permissions
+        fbLogInButton = (LoginButton) findViewById(R.id.btn_login_facebook);
+        fbLogInButton.setReadPermissions("user_friends");
+
+        // Register button callback
+        fbLogInButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(TAG, "onActivityResult() called");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Needed for post-register callback
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.e(TAG, "onStart() called");
+        super.onStart();
+
+        fbLogInButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        Log.e(TAG, "onResume() called");
+        super.onResume();
+
+        // For Facebook App Events
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e(TAG, "onPause() called");
+        super.onPause();
+
+        // For Facebook App Events (deactivation)
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e(TAG, "onDestroy() called");
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        Log.v(TAG, "onCreateOptionsMenu() called");
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        Log.v(TAG, "onOptionsItemSelected() called");
         return super.onOptionsItemSelected(item);
     }
+
 }
