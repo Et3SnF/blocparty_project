0862500fa7ede758c32fbc1a88a27fc7c2af4e74
Steven Nguyen <ngynstvn@gmail.com>
2015-10-27 21:49:03 -0700

    Added dialog fragments and their lifecycle methods (related to collection mode)

---
+++ BlocParty/app/src/main/java/com/ngynstvn/android/blocparty/ui/fragment/CollectionDialog.java
@@ -0,0 +1,91 @@
+package com.ngynstvn.android.blocparty.ui.fragment;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ngynstvn.android.blocparty.BPUtils;
+
+/**
+ * Created by Ngynstvn on 10/27/15.
+ */
+
+public class CollectionDialog extends DialogFragment {
+
+    private static String TAG = BPUtils.classTag(CollectionDialog.class);
+
+    // ----- Lifecycle Methods ------ //
+
+    @Override
+    public void onAttach(Activity activity) {
+        Log.v(TAG, "onAttach() called");
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreate() called");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreateDialog() called");
+        return null;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.v(TAG, "onActivityCreated() called");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.v(TAG, "onStart() called");
+        super.onStart();
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        Log.v(TAG, "onCancel() called");
+        // This is called only if I touched outside of the dialog to dismiss
+        super.onCancel(dialog);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        Log.v(TAG, "onDismiss() called");
+        super.onDismiss(dialog);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.v(TAG, "onSaveInstanceState() called");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        Log.v(TAG, "onStop() called");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.v(TAG, "onDestroyView() called");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.v(TAG, "onDetach() called");
+        super.onDetach();
+    }
+
+    // ----------------------------- //
+
+}
